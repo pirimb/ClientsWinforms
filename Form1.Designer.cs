5792a9c510642570f7c1b379e757b8cac30ad69f
pirimb <pirimb@mail.ru>
2021-10-27 00:46:31 +0400

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace testApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPageFind = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxSelect = new System.Windows.Forms.TextBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.tabPageAdd = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxPin = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.textBoxPatron = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.tabControlReeder = new System.Windows.Forms.TabControl();
+            this.tabPageFilter = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPageFind.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPageAdd.SuspendLayout();
+            this.tabControlReeder.SuspendLayout();
+            this.tabPageFilter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabPageFind
+            // 
+            this.tabPageFind.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPageFind.Controls.Add(this.tableLayoutPanel1);
+            this.tabPageFind.Location = new System.Drawing.Point(4, 22);
+            this.tabPageFind.Name = "tabPageFind";
+            this.tabPageFind.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageFind.Size = new System.Drawing.Size(728, 342);
+            this.tabPageFind.TabIndex = 1;
+            this.tabPageFind.Text = "Find";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxSelect, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSelect, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(722, 336);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(716, 262);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBoxSelect
+            // 
+            this.textBoxSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSelect.Location = new System.Drawing.Point(3, 304);
+            this.textBoxSelect.Name = "textBoxSelect";
+            this.textBoxSelect.Size = new System.Drawing.Size(716, 22);
+            this.textBoxSelect.TabIndex = 2;
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(3, 271);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(120, 27);
+            this.buttonSelect.TabIndex = 0;
+            this.buttonSelect.Text = "Select";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click_1);
+            // 
+            // tabPageAdd
+            // 
+            this.tabPageAdd.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPageAdd.Controls.Add(this.label5);
+            this.tabPageAdd.Controls.Add(this.label6);
+            this.tabPageAdd.Controls.Add(this.label4);
+            this.tabPageAdd.Controls.Add(this.label3);
+            this.tabPageAdd.Controls.Add(this.label2);
+            this.tabPageAdd.Controls.Add(this.label1);
+            this.tabPageAdd.Controls.Add(this.buttonAdd);
+            this.tabPageAdd.Controls.Add(this.textBoxPin);
+            this.tabPageAdd.Controls.Add(this.textBoxPhone);
+            this.tabPageAdd.Controls.Add(this.textBoxMail);
+            this.tabPageAdd.Controls.Add(this.textBoxPatron);
+            this.tabPageAdd.Controls.Add(this.textBoxSurname);
+            this.tabPageAdd.Controls.Add(this.textBoxName);
+            this.tabPageAdd.Location = new System.Drawing.Point(4, 22);
+            this.tabPageAdd.Name = "tabPageAdd";
+            this.tabPageAdd.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAdd.Size = new System.Drawing.Size(728, 342);
+            this.tabPageAdd.TabIndex = 0;
+            this.tabPageAdd.Text = "Add";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(427, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Phone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(533, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Pin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(321, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Mail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Patronimyc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Surname";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(642, 30);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 40);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxPin
+            // 
+            this.textBoxPin.Location = new System.Drawing.Point(536, 41);
+            this.textBoxPin.Name = "textBoxPin";
+            this.textBoxPin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPin.TabIndex = 5;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(430, 41);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPhone.TabIndex = 4;
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.Location = new System.Drawing.Point(324, 41);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMail.TabIndex = 3;
+            // 
+            // textBoxPatron
+            // 
+            this.textBoxPatron.Location = new System.Drawing.Point(218, 41);
+            this.textBoxPatron.Name = "textBoxPatron";
+            this.textBoxPatron.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPatron.TabIndex = 2;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(112, 41);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSurname.TabIndex = 1;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(8, 41);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // tabControlReeder
+            // 
+            this.tabControlReeder.Controls.Add(this.tabPageAdd);
+            this.tabControlReeder.Controls.Add(this.tabPageFind);
+            this.tabControlReeder.Controls.Add(this.tabPageFilter);
+            this.tabControlReeder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabControlReeder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlReeder.Location = new System.Drawing.Point(0, 0);
+            this.tabControlReeder.Name = "tabControlReeder";
+            this.tabControlReeder.SelectedIndex = 0;
+            this.tabControlReeder.Size = new System.Drawing.Size(736, 368);
+            this.tabControlReeder.TabIndex = 0;
+            // 
+            // tabPageFilter
+            // 
+            this.tabPageFilter.Controls.Add(this.label7);
+            this.tabPageFilter.Controls.Add(this.textBoxFilter);
+            this.tabPageFilter.Controls.Add(this.dataGridView2);
+            this.tabPageFilter.Location = new System.Drawing.Point(4, 22);
+            this.tabPageFilter.Name = "tabPageFilter";
+            this.tabPageFilter.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageFilter.Size = new System.Drawing.Size(728, 342);
+            this.tabPageFilter.TabIndex = 2;
+            this.tabPageFilter.Text = "Filter";
+            this.tabPageFilter.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(21, 247);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "name";
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(82, 247);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFilter.TabIndex = 1;
+            this.textBoxFilter.TextChanged += new System.EventHandler(this.textBoxFilter_TextChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(722, 226);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 368);
+            this.Controls.Add(this.tabControlReeder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPageFind.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPageAdd.ResumeLayout(false);
+            this.tabPageAdd.PerformLayout();
+            this.tabControlReeder.ResumeLayout(false);
+            this.tabPageFilter.ResumeLayout(false);
+            this.tabPageFilter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPageFind;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxSelect;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.TabPage tabPageAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxPin;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxMail;
+        private System.Windows.Forms.TextBox textBoxPatron;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TabControl tabControlReeder;
+        private System.Windows.Forms.TabPage tabPageFilter;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace testApp
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ClientsDB"].ConnectionString);
+
+            sqlConnection.Open();
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from Clients", sqlConnection);
+
+            DataSet db = new DataSet();
+
+            dataAdapter.Fill(db);
+
+            dataGridView2.DataSource = db.Tables[0];
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand(
+                $"INSERT INTO Clients (name,surname,patronymic,email,phone,pin) VALUES(@name,@surname,@patronymic,@email,@phone,@pin)",
+                sqlConnection);
+
+            command.Parameters.AddWithValue("name", textBoxName.Text);
+            command.Parameters.AddWithValue("surname", textBoxSurname.Text);
+            command.Parameters.AddWithValue("patronymic", textBoxPatron.Text);
+            command.Parameters.AddWithValue("email", textBoxMail.Text);
+            command.Parameters.AddWithValue("phone", textBoxPhone.Text);
+            command.Parameters.AddWithValue("pin", textBoxPin.Text);
+
+            MessageBox.Show(command.ExecuteNonQuery().ToString());
+        }
+
+
+        private void buttonSelect_Click_1(object sender, EventArgs e)
+        {
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(
+                textBoxSelect.Text,
+                sqlConnection);
+
+            DataSet dataSet = new DataSet();
+
+            dataAdapter.Fill(dataSet);
+
+            dataGridView1.DataSource = dataSet.Tables[0];
+        }
+
+        private void textBoxFilter_TextChanged(object sender, EventArgs e)
+        {
+            (dataGridView2.DataSource as DataTable).DefaultView.RowFilter = $"name like '%{textBoxFilter.Text}%'";
+        }
+    }
+}
